32cc2c7307d0a8c2033899e0144525645c448788
Nikolett Dékány <d.nikolett2@gmail.com>
2025-05-09 18:51:31 +0200

    feat: update project dependencies and configuration - Installed packages: gluestack, tanstack, axios, expo-secure-store, jest, redux, nativewind, tailwindcss - Added new dependencies for Gluestack UI components and utilities. - Updated package.json scripts to support dark mode. - Enhanced TypeScript configuration with path mappings. - Introduced Babel configuration for better module resolution. - Created a new global CSS file for Tailwind styles. - Implemented Gluestack UI provider components for light and dark themes. - Added Tailwind configuration to manage color themes and styles. - Set up Jest configuration for testing with Expo. - Included nativewind environment types for better type support.

---
+++ client/jest.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  preset: "jest-expo",
+
+  setupFiles: ["./jest.setup.js"],
+
+  setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
+
+  transformIgnorePatterns: [
+    "node_modules/(?!(jest-)?react-native" +
+      "|@react-native" +
+      "|@react-navigation" +
+      "|@testing-library" +
+      "|expo" +
+      "|expo-modules-core" +
+      "|expo-font" +
+      "|expo-constants" +
+      "|expo-asset" +
+      "|expo-file-system" +
+      ")",
+  ],
+
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+};
