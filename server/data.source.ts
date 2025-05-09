8add49483fb41301b92254da0e1d60b676c059da
Nikolett Dékány <d.nikolett2@gmail.com>
2025-05-09 19:35:59 +0200

    feat: update package.json and add TypeORM migrations commands
    - Added TypeORM commands for generating, running, showing, and reverting migrations.
    - Updated dependencies including NestJS modules and TypeORM.
    
    refactor: configure TypeORM in app.module.ts
    
    - Imported and configured TypeOrmModule with database options from data.source.ts.
    - Added ConfigModule for global configuration.
    
    chore: enhance main.ts with global validation and CORS
    
    - Implemented global validation pipe for request validation.
    - Enabled CORS for the application.
    
    chore: update tsconfig.json for consistent casing
    
    - Set forceConsistentCasingInFileNames to true for better file handling.
    
    feat: create data.source.ts for database configuration
    
    - Added data source configuration for PostgreSQL using environment variables.
    - Configured entities and migrations paths.
    
    chore: add ESLint configuration for TypeScript
    
    - Created eslint.config.mjs for TypeScript linting with recommended rules.
    - Integrated Prettier with ESLint for code formatting.

---
+++ server/data.source.ts
@@ -0,0 +1,21 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { DataSource, DataSourceOptions } from 'typeorm';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+export const dbConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: +process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  // this should be turned on when testing
+  autoLoadEntities: true,
+  synchronize: false, // Setting synchronize: true shouldn't be used in production - otherwise you can lose production data.
+  entities: ['dist/**/*.entity{.ts,.js}'],
+  migrations: ['dist/src/migrations/*{.ts,.js}'],
+};
+
+const datasource = new DataSource(dbConfig as DataSourceOptions);
+export default datasource;
--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { dbConfig } from '../data.source';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot(dbConfig.options),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
