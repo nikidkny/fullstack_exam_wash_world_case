5037086d1f54a69cba5349f71435920f6a3e7df5
andreaDiClaudio <andreadicla@gmail.com>
2025-05-16 14:30:16 +0200

    set up authorisation and test guardiing route

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,10 +1,17 @@
-import { Controller, Post, Body, HttpStatus } from '@nestjs/common';
+import { Controller, Post, Body, HttpStatus, Get, UseGuards, Request } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login-user.dto';
+import { JwtAuthGuard } from './jwt/jwt.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) { }
+
+  @UseGuards(JwtAuthGuard)
+  @Get("tesGuard")
+  async testGuard(@Request() req) {
+    return { message: 'You have access!', user: req.user };
+  }
 
   @Post('signup')
   async signup(@Body() body) {
@@ -32,4 +39,5 @@ export class AuthController {
       throw error;
     }
   }
+
 }
