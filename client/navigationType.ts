90c3333171c3227671788afcaabdc180a1585226
Nikolett Dékány <d.nikolett2@gmail.com>
2025-05-27 11:53:17 +0200

    added pages to type

--- client/navigationType.ts
+++ client/navigationType.ts
@@ -2,4 +2,10 @@ export type RootStackParamList = {
   Login: undefined;
   Signup: undefined;
   Home: undefined;
+  Profile: undefined;
+  PersonalInfo: undefined;
+  PaymentMethods: undefined;
+  MembershipSettings: undefined;
+  WashHistory: undefined;
+  BillingHistory: undefined;
 };
